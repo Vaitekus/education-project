ac3113a0489a28537260663deaa19cd3cf63dfc0
olga.vaitekus <olga.vaitekus@p2h.com>
2019-08-22 00:36:34 +0300

    Revert "Fixed web pack"
    This reverts commit 96a5c96b708c2f5ee243bbb53fe45cde73afda8e.

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -1,7 +1,11 @@
-const path = require('path');
+const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
+const BUILD_PATH = path.join(__dirname, "dist");
 const MODE = process.env.NODE_ENV || "development";
 
 module.exports = {
@@ -9,8 +13,9 @@ module.exports = {
     devtool: 'source-map',
     entry: path.join(__dirname, "./src/index.js"),
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: '[name].js'
+        filename: "./main.js",
+        path: path.resolve(__dirname, BUILD_PATH),
+        publicPath: ""
     },
     resolve: {
         extensions: [".jsx", ".js"],
@@ -18,37 +23,58 @@ module.exports = {
     },
     module: {
         rules: [
-           {
-                test: /\.scss$/,
-                use: [
-                    {loader: 'style-loader'},
-                    {
-                        loader: 'css-loader',
-                        options: {url: true, sourceMap: true}
-                    },
-                    {
-                        loader: 'postcss-loader', options: {
-                            sourceMap: true,
-                            config: {
-                                path: 'postcss.config.js',
-                            }
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {sourceMap: true}
-                    }
-                ]
+            {
+              test: /\.html$/,
+              use: [
+                {
+                  loader: "html-loader",
+                  options: { minimize: true }
+                }
+              ]
             },
-            {test: /\.(png|jpg|gif)$/, use: ["file-loader?name=[name].[ext]"]}
+            {
+              test: /\.scss$/,               
+              use: [
+                {loader: 'style-loader',},
+                { loader: MiniCssExtractPlugin.loader}, 
+                { 
+                  loader: 'css-loader', 
+                  options: { url: true, sourceMap: true } 
+                },
+                { 
+                  loader: 'sass-loader', 
+                  options: { sourceMap: true } 
+                }, 
+                { 
+                  loader: 'postcss-loader', options: {
+                  sourceMap: true,
+                  config:  {
+                    path: 'postcss.config.js',
+                  }
+                  } 
+                }                   
+              ]
+            },
+            {test: /\.(png|jpg|gif|html)$/, use: ["file-loader?name=[name].[ext]"]}
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({filename: 'index.html', template: 'src/views/index.html'}),
-        new CopyWebpackPlugin([{from: "./src/assets", to: "./assets/"}])
+      new CleanWebpackPlugin(),
+      new HtmlWebPackPlugin({
+        template: "./src/views/index.html",
+        filename: "./index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css"
+      }),
+      new CopyWebpackPlugin([{from: "./src/assets", to: BUILD_PATH + "/assets/"}]),
+      new ExtractTextPlugin("main.css", {options: {allChunks: true}})
     ],
     devServer: {
-        https: false,
-        open: true
+      // contentBase: path.resolve(__dirname, "../src/views/"),
+      compress: false,
+      https: false,
+      open: true,
+      clientLogLevel: 'info'
     }
 };
